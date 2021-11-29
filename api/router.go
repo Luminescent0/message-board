1f127848dfa04d10d06ea97efd36a214bac6af08
WinterBokeh <2082881702@qq.com>
2021-11-29 16:00:36 +0800

    Initial commit

---
+++ api/router.go
@@ -0,0 +1,20 @@
+package api
+
+func InitEngine() {
+	//engine := gin.Default()
+
+	//userGroup := engine.Group("/user")
+	//{
+	//
+	//}
+	//
+	//postGroup := engine.Group("/post")
+	//{
+	//
+	//}
+	//
+	//comment := engine.Group("/comment")
+	//{
+	//
+	//}
+}
