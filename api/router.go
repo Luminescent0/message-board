d2390bacbaa74559008c50df809b96c338b5fd70
WinterBokeh <2082881702@qq.com>
2021-11-29 16:26:45 +0800

    feat(api): router init

--- api/router.go
+++ api/router.go
@@ -1,20 +1,34 @@
 package api
 
-func InitEngine() {
-	//engine := gin.Default()
+import "github.com/gin-gonic/gin"
 
-	//userGroup := engine.Group("/user")
-	//{
-	//
-	//}
-	//
-	//postGroup := engine.Group("/post")
-	//{
-	//
-	//}
-	//
-	//comment := engine.Group("/comment")
-	//{
-	//
-	//}
+func InitEngine() {
+	engine := gin.Default()
+
+	userGroup := engine.Group("/user")
+	{
+		userGroup.POST("/register") //注册
+		userGroup.POST("/login")    //登陆
+		userGroup.POST("/password") //修改密码
+
+		userGroup.POST("/info") //修改个人信息
+		userGroup.GET("/info")  //查看个人信息
+	}
+
+	postGroup := engine.Group("/post")
+	{
+		postGroup.POST("/")         //发布新留言
+		postGroup.POST("/:post_id") //修改留言
+
+		postGroup.GET("/")         //查看全部留言概略
+		postGroup.GET("/:post_id") //查看一条留言详细信息和其下属评论
+	}
+
+	commentGroup := engine.Group("/comment")
+	{
+		commentGroup.POST("/")              //发送评论
+		commentGroup.DELETE("/:comment_id") //删除评论
+	}
+
+	engine.Run()
 }
