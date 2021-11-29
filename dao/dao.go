872d2edcd6f09c1bf4e90f38e73dc7230847a6f2
WinterBokeh <2082881702@qq.com>
2021-11-29 21:24:14 +0800

    fix(db): 修复了无法解析时间的bug

--- dao/dao.go
+++ dao/dao.go
@@ -8,7 +8,7 @@ import (
 var dB *sql.DB
 
 func InitDB() {
-	db, err := sql.Open("mysql", "root:123456@tcp(localhost:3306)/message_board")
+	db, err := sql.Open("mysql", "root:123456@tcp(localhost:3306)/message_board?charset=utf8mb4&parseTime=True")
 	if err != nil {
 		panic(err)
 	}
