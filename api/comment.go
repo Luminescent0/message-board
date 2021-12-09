0d8eca953556e65e48a27afeaaa269d855c9c85b
xian <2460149117@qq.com>
2021-12-09 16:17:30 +0800

    feat(api):新增修改评论的接口，用覆写实现删除评论

--- api/comment.go
+++ api/comment.go
@@ -38,3 +38,55 @@ func addComment(ctx *gin.Context) {
 
 	tool.RespSuccessful(ctx)
 }
+func amendComment(ctx *gin.Context) {
+	iUsername, _ := ctx.Get("username")
+	username := iUsername.(string)
+
+	txt := ctx.PostForm("txt")
+	postIdString := ctx.PostForm("post_id")
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		fmt.Println("post id string to int err:", err)
+		tool.RespErrorWithDate(ctx, "文章id有误")
+		return
+	}
+	comment := model.Comment{
+		PostId:      postId,
+		Txt:         txt,
+		Username:    username,
+		CommentTime: time.Now(),
+	}
+	err = service.AmendComment(comment)
+	if err != nil {
+		fmt.Println("amend comment err : ", err)
+		tool.RespInternalError(ctx)
+		return
+	}
+	tool.RespSuccessful(ctx)
+}
+func deleteComment(ctx *gin.Context) {
+	iUsername, _ := ctx.Get("username") //iUsername为空接口类型
+	username := iUsername.(string)
+
+	txt := "该评论已删除"
+	postIdString := ctx.PostForm("post_id")
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		fmt.Println("post id string to int err", err)
+		tool.RespErrorWithDate(ctx, "文章id有误")
+		return
+	}
+	comment := model.Comment{
+		PostId:      postId,
+		Txt:         txt,
+		Username:    username,
+		CommentTime: time.Now(),
+	}
+	err = service.AmendComment(comment)
+	if err != nil {
+		fmt.Println("delete comment err : ", err)
+		tool.RespInternalError(ctx)
+		return
+	}
+	tool.RespSuccessful(ctx)
+}
